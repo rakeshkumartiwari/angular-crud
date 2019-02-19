4577621089bf22d79e744f3b9381374970987606
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2019-02-20 01:59:16 +0530

    bind dropdown with dynamically

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
   constructor() { }
 
   ngOnInit() {
