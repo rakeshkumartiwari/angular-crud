c1dec1036fb4f80ef7a87d67b93606637300c8ba
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2019-02-21 01:15:14 +0530

    ngx-bootstrap datepicker customisation

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Department } from '../models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,13 +9,18 @@ import { Department } from '../models/department.model';
 })
 export class CreateEmployeeComponent implements OnInit {
 
+  datepickerConfig: Partial<BsDatepickerConfig>;
+
   departments: Department[] = [
     { id: 1, name: 'Help Desk' },
     { id: 2, name: 'HR' },
     { id: 3, name: 'IT' },
     { id: 4, name: 'Payroll' }
   ];
-  constructor() { }
+  constructor() {
+    this.datepickerConfig = Object.assign({}, { containerClass: 'theme-dark-blue', showWeekNumbers: false });
+
+  }
 
   ngOnInit() {
   }
